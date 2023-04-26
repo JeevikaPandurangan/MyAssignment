ffa5357c800ef8c8aedf3a9031ce7feaffe74611
Jeevika P <64584094+JeevikaPandurangan@users.noreply.github.com>
2023-04-26 18:45:49 +0530

    Add files via upload
    Day 2

---
+++ week1/day2/IterationLoop.java
@@ -0,0 +1,33 @@
+package week1.day2;
+
+public class IterationLoop {
+
+	public static void main(String[] args) {
+		
+			//For loop
+			for (int i = 1; i <=10; i++) {
+				System.out.println(i);
+			}
+			
+	        for (int i = 1; i <=10; i++) {
+				System.out.println("For loop --- Jeevika");
+			}
+			
+			//While loop
+			
+			int a=1;
+			while(a<=10) {
+				System.out.println(a);
+				a++;
+			}
+			
+			int b=1;
+			while(b<=5) {
+				System.out.println("While loop --- Jeevika");
+				b++;
+			}
+		}
+
+	}
+
+
---
+++ week1/day2/Selection.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Selection {
+
+	public static void main(String[] args) {
+		
+	//To print Odd Number from 1 to 20
+		for (int i = 1; i <=20 ; i++) {
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
