c323f9c7c8509ab603b1d2140bc86953fe4ba0f5
Jeevika P <64584094+JeevikaPandurangan@users.noreply.github.com>
2023-05-08 13:47:05 +0530

    Add files via upload
    week3day1

---
+++ College.java
@@ -0,0 +1,22 @@
+package org.college;
+
+public class College {
+
+	public void collegeName() {
+		System.out.println("VEC");
+
+	}
+	
+	public void collegeCode() {
+		System.out.println("1132");
+
+	}
+	
+	public void collegeRank() {
+		System.out.println("10th Rank");
+
+	}
+	
+	
+
+}
